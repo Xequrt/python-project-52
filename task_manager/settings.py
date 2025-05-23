030f871cad0a1d97a3214de2fe25c1752434f86a
Дмитрий Каблучко <xequrt@yandex.ru>
2025-05-23 14:22:14 +1000

    Add env production PostgreSQL

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', 'db.sqlite3'),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
     }
 }
 
