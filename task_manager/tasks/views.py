9e7b0b4bd68f4ebb5a7877c8d716cdb2643a84d8
Дмитрий Каблучко <xequrt@yandex.ru>
2025-06-06 20:13:27 +1000

    Fix lint

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
