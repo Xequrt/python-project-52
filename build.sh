4a04d7939fe81c9c3a81f88bec899cc32a59b692
Дмитрий Каблучко <xequrt@yandex.ru>
2025-06-18 23:18:47 +1000

    Add compilemessage into build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ source $HOME/.local/bin/env
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
+make compilemessages
 make install && make collectstatic && make migrate
 
 SUPERUSER_EXISTS=$(python3 manage.py shell -c \
